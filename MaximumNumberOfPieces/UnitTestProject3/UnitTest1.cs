9f1c261cdcd2676ebe0bcd2b0f5281be033c321b
Akashris <akashritharan@gmail.com>
2020-01-27 19:20:08 +0530

    Updated

--- MaximumNumberOfPieces/UnitTestProject3/UnitTest1.cs
+++ MaximumNumberOfPieces/UnitTestProject3/UnitTest1.cs
@@ -16,6 +16,10 @@ namespace UnitTestProject3
             {1, 2, 3, 4, 5};
             int actual = obj.CalculationMaximumPieces(numberOfPoints);
             List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(2);
+            List<BigInteger> expected.Add(4);
+            List<BigInteger> expected.Add(8);
+            List<BigInteger> expected.Add(16);
             Assert.AreEqual(expected, actual);
         }
         public void TestMethod2()
@@ -24,7 +28,11 @@ namespace UnitTestProject3
             List<BigInteger> numberOfPoints = new List<BigInteger>()
             {1, 1, 2, -4, 3};
             int actual = obj.CalculationMaximumPieces(numberOfPoints);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(2);
             List<BigInteger> expected.Add(-1);
+            List<BigInteger> expected.Add(4);
             Assert.AreEqual(expected, actual);
         }
         public void TestMethod3()
@@ -34,6 +42,10 @@ namespace UnitTestProject3
             {0,0,0,0,0};
             int actual = obj.CalculationMaximumPieces(numberOfPoints);
             List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(1);
             Assert.AreEqual(expected, actual);
         }
         public void TestMethod4()
@@ -42,6 +54,23 @@ namespace UnitTestProject3
             List<BigInteger> numberOfPoints = new List<BigInteger>()
             {0, -1, 2, 4, 3};
             int actual = obj.CalculationMaximumPieces(numberOfPoints);
+            List<BigInteger> expected.Add(1);
+            List<BigInteger> expected.Add(-1);
+            List<BigInteger> expected.Add(2);
+            List<BigInteger> expected.Add(8);
+            List<BigInteger> expected.Add(4)
+            Assert.AreEqual(expected, actual);
+        }
+        public void TestMethod5()
+        {
+            EllipticalLand obj = new EllipticalLand();
+            List<BigInteger> numberOfPoints = new List<BigInteger>()
+            {-1,-2,-3,-4,-5};
+            int actual = obj.CalculationMaximumPieces(numberOfPoints);
+            List<BigInteger> expected.Add(-1);
+            List<BigInteger> expected.Add(-1);
+            List<BigInteger> expected.Add(-1);
+            List<BigInteger> expected.Add(-1);
             List<BigInteger> expected.Add(-1);
             Assert.AreEqual(expected, actual);
         }
